4129dc2e18b2c9da3d3399cd77bf419b90a1693b
Romann1234 <145775621+Romann1234@users.noreply.github.com>
2023-09-25 18:18:01 +0500

    Add files via upload

--- p1/Main.java
+++ p1/Main.java
@@ -5,13 +5,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+
         Scanner in = new Scanner(System.in);
         System.out.println("chislo 1");
         int num = in.nextInt();
-        System.out.println("chislo 2");
+        System.out.println("chsilo 2");
         int num2 = in.nextInt();
+        System.out.println("viberite operaciu: 1.+ 2.- 3./ 4.* ");
+        int num3 = in.nextInt();
+        switch (num3){
+            case 1:
+                System.out.printf("+: %d",num+num2);
+                break;
+            case 2:
+                System.out.printf("-: %d",num-num2);
+                break;
+            case 3:
+                System.out.printf("/: %d",num/num2);
+                break;
+            case 4:
+                System.out.printf("*: %d",num*num2);
+                break;
+            default:
+                System.out.println("not found");
+        }
 
-        System.out.printf("+: %d \n-: %d\n /: %d \n *: %d \n ", num + num2,num - num2,num/num2, num*num2);
 
         in.close();
 
